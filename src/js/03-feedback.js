2f03d317040d1284d1bce771757339b604a673dc
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-02-26 20:21:12 +0200

    made task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    email: document.querySelector(".feedback-form input"),
+    message: document.querySelector(".feedback-form textarea")
+}
+let formData = {};
+
+const STORAGE_KEY = "feedback-form-state";
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInputCreateFormData, 500));
+
+function onInputCreateFormData(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+function populateForm() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    console.log(savedMessage)
+
+    const parsedMessage = JSON.parse(savedMessage);
+    
+    if (parsedMessage) { 
+        refs.inputEmail.value = parsedMessage.email;
+        refs.textMessage.value = parsedMessage.message;
+    };
+
+    console.log(parsedMessage);
+}
 
